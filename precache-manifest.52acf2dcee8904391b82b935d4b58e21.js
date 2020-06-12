e319720cbca4b2f5f1094477b455463c0f7adaad
Sai Krishna <saikrishna100797@gmail.com>
2020-06-12 11:57:51 +0530

    Updates

---
+++ precache-manifest.52acf2dcee8904391b82b935d4b58e21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878418228c9c5029ef4191040247d82f",
+    "url": "/post-it/index.html"
+  },
+  {
+    "revision": "b70c66e9fe269be11a83",
+    "url": "/post-it/static/js/2.c356a3cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/post-it/static/js/2.c356a3cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b6b9d3cf190a35af977",
+    "url": "/post-it/static/js/main.157e88bf.chunk.js"
+  },
+  {
+    "revision": "c7919b7a448d7d714a9d",
+    "url": "/post-it/static/js/runtime-main.f469793c.js"
+  }
+]);
